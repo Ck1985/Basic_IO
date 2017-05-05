da92def3f151e613e4d51eb2d4e7f8f2300d4910
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-05-06 00:49:51 +0700

    New Commit

--- src/introducing_IO/ConsoleDemo.java
+++ src/introducing_IO/ConsoleDemo.java
@@ -21,9 +21,21 @@ public class ConsoleDemo {
         }
     }
     public static void readLineVer2() {
-
+        Console console = null;
+        String line = null;
+        try {
+            console = System.console();
+            if (console != null) {
+                line = console.readLine("Hi %s, How are you ? %n" +
+                "I known your information: Your age: %d, weight: %f. You like prograaming: %b", "anonymous", 31, 70.5f, true);
+                System.out.println("The result: " + line);
+            }
+        } catch (IOError error) {
+            error.printStackTrace();
+        }
     }
     public static void main(String[] args) {
-
+        //readLineVer1();
+        readLineVer2();
     }
 }
