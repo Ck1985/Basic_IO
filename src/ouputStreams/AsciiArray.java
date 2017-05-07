d3800d760d5ff39cb125b9a885d2d3892eb67527
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-05-07 15:49:44 +0700

    New Commit

---
+++ src/ouputStreams/AsciiArray.java
@@ -0,0 +1,31 @@
+package ouputStreams;
+
+import java.io.IOException;
+
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+public class AsciiArray {
+    public static void main(String[] args) {
+        String s = "How are streams treating you ?";
+        byte[] data = s.getBytes();
+        //System.out.write(data);
+        System.out.println("-------------------------");
+        byte[] b = new byte[(127 - 31) * 2];
+        int index = 0;
+        for (int i = 32; i < 127; i++) {
+            b[index++] = (byte)i;
+            if (i % 8 == 0) {
+                b[index++] = (byte)'\n';
+            } else {
+                b[index++] = (byte)'\t';
+            }
+        }
+        b[index++] = (byte)'\n';
+        try {
+            System.out.write(b);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
--- src/ouputStreams/AsciiChart.java
+++ src/ouputStreams/AsciiChart.java
@@ -6,9 +6,9 @@ import java.io.OutputStream;
  */
 public class AsciiChart {
     public static void main(String[] args) {
-        for (int i = 8; i < 127; i++) {
+        for (int i = 32; i < 127; i++) {
             System.out.write(i);
-            if (i % 8 == 0) {
+            if (i % 8 == 7) {
                 //System.out.write('\n');
                 System.out.write(10);
             } else {
