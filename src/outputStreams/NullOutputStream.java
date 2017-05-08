6c169f3ca143ebb9c3294d3f3fd989b9768a1ac6
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-05-08 21:52:20 +0700

    New Commit

---
+++ src/outputStreams/NullOutputStream.java
@@ -0,0 +1,29 @@
+package outputStreams;
+
+import java.io.*;
+
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+public class NullOutputStream extends OutputStream {
+    private boolean closed = false;
+    public void write(int b) throws IOException{
+        if (closed) {
+            throw new IOException("Write to the closed stream");
+        }
+    }
+    public void write(byte[] data, int offset, int length) throws IOException {
+        if (data == null) {
+            throw new NullPointerException("data is null");
+        }
+        if (closed) {
+            throw new IOException("Write to the closed stream");
+        }
+    }
+    public void close() {
+        this.closed = true;
+    }
+    public static void main(String[] args) {
+
+    }
+}
