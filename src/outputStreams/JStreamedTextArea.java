2eef489d3c29743f8eb3f870dee2a852a7016cff
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-05-09 16:53:02 +0700

    New Commit

---
+++ src/outputStreams/JStreamedTextArea.java
@@ -0,0 +1,13 @@
+package outputStreams;
+
+import java.io.*;
+import javax.swing.*;
+
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+class TextAreaOutputStream extends OutputStream {
+
+}
+public class JStreamedTextArea extends JTextArea {
+}
