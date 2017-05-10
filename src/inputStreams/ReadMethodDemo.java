992ba3b5828fe3ed82f5f75044bbf9d175825cf4
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-05-10 22:57:43 +0700

    New Commit

--- src/inputStreams/ReadMethodDemo.java
+++ src/inputStreams/ReadMethodDemo.java
@@ -8,16 +8,23 @@ import java.util.Arrays;
  */
 public class ReadMethodDemo {
     public static void main(String[] args) throws IOException{
+        /**
         int[] data = new int[10];
         for (int i = 0; i < data.length; i++) {
             data[i] = System.in.read();
         }
         System.out.println(Arrays.toString(data));
+         */
 
         byte[] bytes = new byte[10];
+        int[] convert = new int[10];
+        int index = 0;
         for (int i = 0; i < bytes.length; i++) {
             bytes[i] = (byte)System.in.read();
+            if (bytes[i] < 0) {
+                convert[index] = bytes[i] + 256;
+            }
         }
-        System.out.println(Arrays.toString(bytes));
+        System.out.println(Arrays.toString(convert));
     }
 }
